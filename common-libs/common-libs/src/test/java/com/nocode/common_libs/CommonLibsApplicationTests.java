ff00137d3d756616faf3429ed110045024c94592
Pranay <darnepranay@gmail.com>
2025-10-20 01:36:12 +0530

    new projects added

---
+++ common-libs/common-libs/src/test/java/com/nocode/common_libs/CommonLibsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocode.common_libs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
